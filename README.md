9737aa8af6c3c88014289efc9da0e050bf2883e3
FranciscoFaundez <panchooryo@gmail.com>
2025-03-29 14:54:08 -0300

    Hay un snoopy  que se enoja cuando se pega

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Para ejecutar, corra python3 main.py
+
 En caso de que te falte alguna librería, correr:
 pip install -r requirements.txt
--- main.py
+++ main.py
@@ -7,16 +7,24 @@ import aux_functions as af
 import random
 import square as sq
 
-
-# Opcionalmente seteamos variables para el tamaño
+# Seteamos variables para el tamaño
 WIDTH = 640
 HEIGHT = 640
 
-# Asigno valores iniciales a la velocidad del círculo y cuadrado de forma random
+# Tamaño imagen
+WIDTH_img = 100
+HEIGHT_img = 100
+# Tamaño inicial de la imagen
+initial_scale = 0.5 
+
+
+# Asigno valores iniciales a la velocidad del círculo, cuadrado e imagen de forma random
 VEL_X_cl = random.uniform(-0.03, 0.03)
 VEL_Y_cl = random.uniform(-0.03, 0.03)
 VEL_X_sq = random.uniform(-0.03, 0.03)
 VEL_Y_sq = random.uniform(-0.03, 0.03)
+VEL_X_img = random.choice([-3, -2, 2, 3])
+VEL_Y_img = random.choice([-3, -2, 2, 3])
 
 # Cantidad de triángulos a usar para armar el círculo
 DEFINITION = 100
@@ -38,43 +46,66 @@ if __name__ == "__main__":
                             height=HEIGHT, resizable=True)
     # Creamos nuestros shaders
     vertex_source = """
-#version 330
+    #version 330
 
-in vec3 position;
-in vec3 color;
+    in vec3 position;
+    in vec3 color;
 
-out vec3 fragColor;
+    out vec3 fragColor;
 
-void main() {
-    fragColor = color;
-    gl_Position = vec4(position, 1.0f);
-}
+    void main() {
+        fragColor = color;
+        gl_Position = vec4(position, 1.0f);
+    }
     """
 
     fragment_source = """
-#version 330
+    #version 330
 
-in vec3 fragColor;
-out vec4 outColor;
+    in vec3 fragColor;
+    out vec4 outColor;
 
-void main()
-{
-    outColor = vec4(fragColor, 1.0f);
-}
+    void main()
+    {
+        outColor = vec4(fragColor, 1.0f);
+    }
+        """
+
+    vertex_source_code_img = """
+        #version 330
+
+        in vec3 position;
+        in vec3 color;
+        in float intensity;
+        uniform mat4 transform;
+
+
+        out vec3 fragColor;
+        out float fragIntensity;
+
+        void main()
+        {
+            fragColor = color;
+            fragIntensity = intensity;
+            gl_Position = vec4(position, 1.0f);
+        }
     """
 
     # Compilamos los shaders
-    vert_program = pyglet.graphics.shader.Shader(vertex_source, "vertex")
-    frag_program = pyglet.graphics.shader.Shader(fragment_source, "fragment")
+    vert_shader = pyglet.graphics.shader.Shader(vertex_source, "vertex")
+    vert_shader_img = pyglet.graphics.shader.Shader(vertex_source_code_img, "vertex")
+    frag_shader = pyglet.graphics.shader.Shader(fragment_source, "fragment")
 
     # Creamos nuestro pipeline de rendering
-    pipeline = pyglet.graphics.shader.ShaderProgram(vert_program, frag_program)
+    pipeline = pyglet.graphics.shader.ShaderProgram(vert_shader, frag_shader)
+    pipelineimg = pyglet.graphics.shader.ShaderProgram(vert_shader_img, frag_shader)
+
 
     # Creamos el circulo
     circle_pos = cl.create_circle(0.0, 0.0, 0.15, DEFINITION)
 
     # Creamos el circulo en la gpu, ahora con menos vertices en total
-    # y le tenemos que pasar los indices
+    # y le tenemos que pasar los índices
     circle_gpu = pipeline.vertex_list_indexed(DEFINITION+1, GL_TRIANGLES, cl.create_circle_indices(DEFINITION))
 
     # Copiamos los datos, añadimos el color
@@ -83,13 +114,19 @@ void main()
     circle_gpu.color[:] = af.randomize_color(DEFINITION+1)
 
     # Ahora creamos el cuadrado
-    # Creamos el cuadrado en la gpu
     square_gpu = pipeline.vertex_list_indexed(4, GL_TRIANGLES, sq.indices)
-    # Copiamos los datos, añadimos el color
+    # Asignamos la posición
     square_gpu.position[:] = sq.positions
     # Le asigno un color inicial al azar al cuadrado
     square_gpu.color[:] = af.randomize_color(4)
 
+    # Cargamos la imagen
+    image1 = pyglet.image.load("snoopy_happy.png")
+    image2 = pyglet.image.load("snoopy_angry.png")
+
+    # Crear sprite con posición inicial y escala
+    sprite = pyglet.sprite.Sprite(image1, x = WIDTH_img, y = HEIGHT_img)
+    sprite.scale = initial_scale
 
     @window.event
     def on_draw():
@@ -101,6 +138,9 @@ void main()
         pipeline.use()
         circle_gpu.draw(GL_TRIANGLES)
         square_gpu.draw(GL_TRIANGLES)
+        
+        pipelineimg.use()
+        sprite.draw()
 
     # Función para mover el círculo
     def move_circle(dt):
@@ -120,8 +160,54 @@ void main()
         square_gpu.position[:], VEL_X_sq, VEL_Y_sq = sq.move_sq(square_gpu.position[:], VEL_X_sq, VEL_Y_sq)
 
 
+
+    # HAREMOS QUE UN SNOOPY SE ENOJE CADA VEZ QUE SE GOLPEE # 
+
+    # timer_event me indicará si ya hay algún schedule_once corriendo, para poder pararlo y generar uno nuevo
+    timer_event = False
+    # Función para volver a la imagen original de snoopy feliz
+    def reset_image(dt):
+        global timer_event,sprite
+        # Cancelar el temporizador si ya existe
+        if timer_event:
+            pyglet.clock.unschedule(timer_event)
+        # Asignar la imagen original
+        sprite.scale = initial_scale
+        sprite.image = image1
+
+    # Función para el movimiento de la imagen
+    def move_image(dt):
+        global sprite, VEL_X_img, VEL_Y_img, timer_event
+
+        # Muevo la imagen
+        sprite.x += VEL_X_img 
+        sprite.y += VEL_Y_img
+
+        # Rebotar en los bordes y cambiar de imagen
+        if sprite.x + sprite.width  > window.width or sprite.x < 0:
+            timer_event = True
+            # Cambio el sentido del movimiento
+            VEL_X_img = -VEL_X_img
+            sprite.scale = 0.08
+            sprite.image = image2
+            # Luego de medio segundo, se vuelve a la imagen original
+            pyglet.clock.schedule_once(reset_image, 0.5)  # Programar el reinicio en 1 segundo
+
+
+        if sprite.y + sprite.height> window.height or sprite.y < 0:
+            timer_event = True
+            VEL_Y_img = -VEL_Y_img
+            sprite.scale = 0.08
+            sprite.image = image2
+            pyglet.clock.schedule_once(reset_image, 0.5)  # Programar el reinicio en 1 segundo
+
+
+            
+
+
     pyglet.clock.schedule_interval(move_circle, 1/60)
     pyglet.clock.schedule_interval(move_square, 1/60)
+    pyglet.clock.schedule_interval(move_image, 1/60)
 
 
     pyglet.app.run()
